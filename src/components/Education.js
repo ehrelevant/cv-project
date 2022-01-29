a2625cd935f3e882c499289e380f5f9f464a26c8
KindaExists <ehrencastillo@gmail.com>
2022-01-29 20:03:53 +0800

    add input and form handling to Education

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,47 +6,68 @@ class Education extends Component {
   }
 
   render() {
+    const { onEducationChange, educationalEntry } = this.props;
+    const entryId = educationalEntry.id;
+
     return (
-      <div className="cv-section">
-        <div class="label-input-pair">
-          <label>University name</label>
+      <section className="cv-section">
+        <div className="label-input-pair">
+          <label htmlFor={"university"+entryId}>University name</label>
           <input
             type="text"
+            name={"university"+entryId}
+            value={educationalEntry.university}
+            onChange={evt => onEducationChange(evt, educationalEntry.id)}
           />
         </div>
-        <div class="label-input-pair">
-          <label>Degree</label>
+        <div className="label-input-pair">
+          <label htmlFor={"address"+entryId}>University address</label>
           <input
             type="text"
+            name={"address"+entryId}
+            value={educationalEntry.address}
+            onChange={evt => onEducationChange(evt, educationalEntry.id)}
           />
         </div>
-        <div class="label-input-pair">
-          <label>University address</label>
+        <div className="label-input-pair">
+          <label htmlFor={"degree"+entryId}>Degree</label>
           <input
             type="text"
+            name={"degree"+entryId}
+            value={educationalEntry.degree}
+            onChange={evt => onEducationChange(evt, educationalEntry.id)}
           />
         </div>
-        <div class="label-input-pair">
-          <label>Start date</label>
+        <div className="label-input-pair">
+          <label htmlFor="gpa">GPA</label>
+          <input
+            type="text"
+            name={"gpa"+entryId}
+            value={educationalEntry.gpa}
+            onChange={evt => onEducationChange(evt, educationalEntry.id)}
+          />
+        </div>
+        <div className="label-input-pair">
+          <label htmlFor={"startDate"+entryId}>Start date</label>
           <input
             type="text"
             placeholder="Month & Year"
+            name={"startDate"+entryId}
+            value={educationalEntry.startDate}
+            onChange={evt => onEducationChange(evt, educationalEntry.id)}
           />
         </div>
-        <div class="label-input-pair">
-          <label>End date</label>
+        <div className="label-input-pair">
+          <label htmlFor={"endDate"+entryId}>End date</label>
           <input
             type="text"
             placeholder="Month & Year (or Present)"
+            name={"endDate"+entryId}
+            value={educationalEntry.endDate}
+            onChange={evt => onEducationChange(evt, educationalEntry.id)}
           />
         </div>
-        <div class="label-input-pair">
-          <label>GPA</label>
-          <input
-            type="text"
-          />
-        </div>
-      </div>
+      </section>
     );
   }
 }
