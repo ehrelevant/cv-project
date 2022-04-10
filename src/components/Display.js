0ae22c56449dc3cd731addd984a38a629e07ed13
KindaExists <ehrencastillo@gmail.com>
2022-04-11 00:20:30 +0800

    fix cv display layout

--- src/components/Display.js
+++ src/components/Display.js
@@ -17,20 +17,20 @@ class Display extends Component {
               <h2 className='cv-header-title'>Cool Prison Man</h2>
               <div className='cv-general-info'>
                 <div className='cv-general-entry cv-address'>
-                  <h3>Address: </h3>
+                  <h3 className="cv-general-title">Address: </h3>
                   <p>16 John St.</p>
                   <p>Sad City, Philippines</p>
                 </div>
                 <div className='cv-general-entry cv-phone'>
-                  <h3>Phone: </h3>
+                  <h3 className="cv-general-title">Phone: </h3>
                   <p>1234567890</p>
                 </div>
                 <div className='cv-general-entry cv-email'>
-                  <h3>Email: </h3>
+                  <h3 className="cv-general-title">Email: </h3>
                   <p>kindaexists@gmail.com</p>
                 </div>
                 <div className='cv-general-entry cv-website'>
-                  <h3>Website: </h3>
+                  <h3 className="cv-general-title">Website: </h3>
                   <p><a href='https://github.com/KindaExists' target='_blank' rel='noreferrer'>https://github.com/KindaExists</a></p>
                 </div>
               </div>
@@ -40,7 +40,7 @@ class Display extends Component {
           <main className="cv-body">
 
             <div className="cv-profile">
-              <h3>Profile</h3>
+              <h3 className="cv-body-title">Profile</h3>
               <p>
                 I am Jean Valjean.
                 Do consequat ea qui sint velit ullamco officia incididunt tempor et velit. Nostrud ut irure occaecat duis pariatur ullamco aute elit aliqua enim amet qui reprehenderit. Consectetur mollit elit et excepteur esse incididunt ipsum.
@@ -48,8 +48,32 @@ class Display extends Component {
               </p>
             </div>
             <div className="cv-education">
-              <h3 className="cv-section-title">Education</h3>
+              <h3 className="cv-body-title">Education</h3>
               <div className="educational-entries">
+
+                <div className="educational-entry">
+                  <div className="entry-left">
+                    <p className="generic-entry-date">June 2016 - Mar 2018</p>
+                  </div>
+                  <div className="entry-right">
+                    <p className="educational-entry-degree">Bachelor in Computer Things</p>
+                    <p className="educational-entry-university">Universe University</p>
+                    <p className="educational-entry-address">CT City, Philippines</p>
+                    <p className="educational-entry-gpa">GPA: 4.00</p>
+                  </div>
+                </div>
+
+                <div className="educational-entry">
+                  <div className="entry-left">
+                    <p className="generic-entry-date">June 2016 - Mar 2018</p>
+                  </div>
+                  <div className="entry-right">
+                    <p className="educational-entry-degree">Bachelor in Computer Things</p>
+                    <p className="educational-entry-university">Universe University</p>
+                    <p className="educational-entry-address">CT City, Philippines</p>
+                    <p className="educational-entry-gpa">GPA: 4.00</p>
+                  </div>
+                </div>
 
                 <div className="educational-entry">
                   <div className="entry-left">
@@ -66,7 +90,7 @@ class Display extends Component {
               </div>
             </div>
             <div className="cv-practical">
-              <h3 className="cv-section-title">Practical Experience</h3>
+              <h3 className="cv-body-title">Practical Experience</h3>
               <div className="practical-entries">
 
                 <div className="practical-entry">
